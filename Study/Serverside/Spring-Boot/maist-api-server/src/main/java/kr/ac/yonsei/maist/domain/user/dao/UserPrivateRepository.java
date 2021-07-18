bd25c7a32a538d1b7997d319dc696875ac6cfb9d
chanwoo kwon <arknell@yonsei.ac.kr>
2021-07-18 18:48:11 +0900

    delete all

--- Study/Serverside/Spring-Boot/maist-api-server/src/main/java/kr/ac/yonsei/maist/domain/user/dao/UserPrivateRepository.java
+++
@@ -1,8 +0,0 @@
-package kr.ac.yonsei.maist.domain.user.dao;
-
-import kr.ac.yonsei.maist.domain.user.domain.UserPrivate;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserPrivateRepository extends JpaRepository<UserPrivate, Integer> {
-
-}
--- Study/Serverside/Spring-Boot/maist-api-server/src/main/java/kr/ac/yonsei/maist/domain/user/dao/UserPublicRepository.java
+++
@@ -1,8 +0,0 @@
-package kr.ac.yonsei.maist.domain.user.dao;
-
-import kr.ac.yonsei.maist.domain.user.domain.UserPublic;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserPublicRepository extends JpaRepository<UserPublic, Integer> {
-
-}
